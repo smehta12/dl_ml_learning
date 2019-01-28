6502773d032c20330e5466ef694f9d4bd90f7df9
smehta12 <shalinmehta85@gmail.com>
2019-01-28 08:27:34 -0800

    Delete lanes.py

--- cv_basics/finding_lanes/lanes.py
+++
@@ -1,9 +0,0 @@
-# learning of the opencv basics from the https://www.youtube.com/watch?v=eLTLtUVuuy4
-
-import cv2
-
-# returns the np array
-image  = cv2.imread('test_image.jpg')
-
-cv2.imshow('result', image)
-cv2.waitKey(0) # if 0 display window infinitly until an key pressed else add milliseconds in arg
